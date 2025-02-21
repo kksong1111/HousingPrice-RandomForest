393add9e21a33ea1e5e2fee5a4fe8fe92a4c6cdc
kksong1111 <103341270+kksong1111@users.noreply.github.com>
2025-02-21 08:24:10 -0500

    Add files via upload

--- Models.R
+++ Models.R
@@ -49,6 +49,29 @@ results <- rbind(results, data.frame(Model = "Linear Regression", num_trees = NA
 # Different num.trees for Random Forest
 num_trees_values <- c(50, 80, 100, 200)
 
+
+# Handle missing values in training data
+train_data_rf[sapply(train_data_rf, is.numeric)] <- lapply(train_data_rf[sapply(train_data_rf, is.numeric)], function(x) {
+  ifelse(is.na(x), median(x, na.rm = TRUE), x)
+})
+
+train_data_rf[sapply(train_data_rf, is.character)] <- lapply(train_data_rf[sapply(train_data_rf, is.character)], function(x) {
+  x[is.na(x)] <- "Missing"
+  return(as.factor(x))
+})
+
+# Handle missing values in test data
+test_features_rf[sapply(test_features_rf, is.numeric)] <- lapply(test_features_rf[sapply(test_features_rf, is.numeric)], function(x) {
+  ifelse(is.na(x), median(x, na.rm = TRUE), x)
+})
+
+test_features_rf[sapply(test_features_rf, is.character)] <- lapply(test_features_rf[sapply(test_features_rf, is.character)], function(x) {
+  x[is.na(x)] <- "Missing"
+  return(as.factor(x))
+})
+
+
+
 for (num_trees in num_trees_values) {
   # Random forest model
   bag.y <- ranger(SalePrice ~ ., data = train_data_rf, mtry = ncol(train_data_rf) - 1, num.trees = num_trees, respect.unordered.factors = "order")
